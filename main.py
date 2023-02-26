dd4af837c99eaeb7b27dab0d5c8843914d081707
RedxMime <105324123+RedxMime@users.noreply.github.com>
2023-02-26 17:06:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import subprocess
 
-dir = "путь к папке в которй запущен бот"
+dir = "Spam"
 
 for token in open(f"{dir}emailpass.txt","r").read().splitlines():
     subprocess.Popen(['python3',f'{dir}bot.py',token.split(':')[0],token.split(':')[1]])
